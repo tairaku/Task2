f8630174c720b93887a102b7dc58fe23213cfad5
tsumugi <tsumugi.1218@gmail.com>
2015-06-24 17:40:47 +0900

    5

--- app/src/main/java/com/lifeistech/android/task2/AddListActivity.java
+++ app/src/main/java/com/lifeistech/android/task2/AddListActivity.java
@@ -1,39 +1,32 @@
 package com.lifeistech.android.task2;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.util.Date;
 
 
-public class List2Activity extends ActionBarActivity {
+
+public class AddListActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list2);
-        String[] array ={"Apple","Banana","Orange"};
-        ArrayAdapter arrayadapter=new ArrayAdapter(this,R.layout.rowdata2,array);
-        ListView listview =(ListView)findViewById(R.id.listView);
-        listview.setAdapter(arrayadapter);
+        setContentView(R.layout.activity_add_list);
     }
 
-    public void back(View v){
-        finish();
-    }
-
-    public void addlist(View v){
-        Intent intent=new Intent(this,AddListActivity.class);
-        startActivity(intent);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list2, menu);
+        getMenuInflater().inflate(R.menu.menu_add_list, menu);
         return true;
     }
 
