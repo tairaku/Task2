ff94f319d5dfc3b6d031dd45e800dceb229133ea
tsumugi <tsumugi.1218@gmail.com>
2015-07-01 01:35:23 +0900

    クリックしたリストのアイテムがとれない…＞＜

--- app/src/main/java/com/lifeistech/android/task2/List2Activity.java
+++ app/src/main/java/com/lifeistech/android/task2/List2Activity.java
@@ -13,21 +13,35 @@ import java.util.*;
 
 public class List2Activity extends Activity {
 
+    Homework homework;
     ListView listview;
-    MyListAdapter adapter;
-
+    ListAdapter adapter;
+    Homework remove;
     private SharedPreferences date_sp2;
     private Editor date_editor2;
+    int month2;
+    int day2;
+    String title2;
+    static int item_id;
+    static String item_id2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list2);
+        //ArrayAdapter<String> array3=AddListActivity.adapter2;
+        List<Homework> items = new ArrayList<Homework>();
 
-        listview = (ListView) findViewById(R.id.listView);
+        homework=new Homework();
+        remove=new Homework();
 
         date_sp2 = getSharedPreferences("date2", MODE_PRIVATE);
         date_editor2 = date_sp2.edit();
+
+        listview =(ListView)findViewById(R.id.listView);
+
+
 
         adapter = new MyListAdapter(this, R.layout.rowdata, AddListActivity.dataList2);
         listview.setAdapter(adapter);
@@ -40,36 +54,42 @@ public class List2Activity extends Activity {
                                     View view, int pos, long id) {
 
                 // 選択アイテムを取得
-                Homework work = adapter.getItem(pos);
-                String workDay = "" + work.getDay();
-
-                date_editor2.putString("workDay", workDay);
+                ListView listView2 = (ListView) parent;
+                String item = (String) listView2.getItemAtPosition(pos);
+                //itemid = Integer.parseInt(item);
+                //item_id=pos;
+                //item_id=((ListView) parent).getItemAtPosition(pos);
+                item_id2=(String)((ListView) parent).getItemAtPosition(pos);
+                date_editor2.putString("item_id2", item_id2);
                 date_editor2.commit();
+
+
             }
         });
     }
 
-    public void back(View v) {
-        Intent intent = new Intent(this, MainActivity.class);
+    public void back(View v){
+        Intent intent=new Intent(this,MainActivity.class);
         startActivity(intent);
     }
 
-    public void addlist(View v) {
-        Intent intent2 = new Intent(this, AddListActivity.class);
+    public void addlist(View v){
+        Intent intent2=new Intent(this,AddListActivity.class);
         startActivity(intent2);
     }
 
 
-    public void goTopart(View v) {
+    public void goTopart(View v){
         //date_editor2.putInt("item_id", item_id);
         //date_editor2.commit();
         //date_editor2.putString("title2", title2);
         //date_editor2.putInt("month2", month2);
         //date_editor2.putInt("day2", day2);
         //date_editor2.commit();
-        Intent intent3 = new Intent(this, ListDetails.class);
+        Intent intent3=new Intent(this,ListDetails.class);
         startActivity(intent3);
     }
+
 
 
     @Override
